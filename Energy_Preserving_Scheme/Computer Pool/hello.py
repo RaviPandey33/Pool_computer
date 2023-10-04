038289266cd1c9d904d20cfeba9ebfac58f00562
Ravi <ravipandey0706@gmail.com>
2023-10-04 23:09:28 +0200

    ..

--- Energy_Preserving_Scheme/Computer Pool/hello.py
+++ Energy_Preserving_Scheme/Computer Pool/hello.py
@@ -1,11 +1,15 @@
+import os
+print(os.getcwd())
+
 import json
+
 
 # Open the existing JSON file in read mode
 with open('hello_output.json', 'r') as file:
     numbers = json.load(file)
 
 # Append new numbers to the list
-for i in range(1, 100):
+for i in range(100, 1000):
     numbers.append(i)
 
 # Write the updated list back to the JSON file
